ed33cb7115a263f0761c8dd52637f17a133709f0
Akshit Kumar <akshitkumar450@gmail.com>
2021-04-02 18:25:04 +0530

    helper funtion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,12 +1,20 @@
 import React from 'react'
 
 const Accordion = (props) => {
-    const items = props.items.map((item) => {
+
+    const onTitleClicked = (index) => {
+        console.log('title clicked', index)
+    }
+
+    const items = props.items.map((item, index) => {
         return (
             <div key={item.title}>
-                <div className='title active'>
+                <div
+                    className='title active'
+                    onClick={() => onTitleClicked(index)}
+                >
                     <i className='dropdown icon'></i>
-                    <h1>{item.title}</h1>
+                    {item.title}
                 </div>
 
                 <div className='content active'>
