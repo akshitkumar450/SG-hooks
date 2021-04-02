267ffaf4fae45d0ae6033f3f832748ca5b02ed84
Akshit Kumar <akshitkumar450@gmail.com>
2021-04-02 22:30:52 +0530

    making api request

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,27 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Search = () => {
     const [term, setTerm] = useState('')
 
-    console.log('i run every render');
-
+    // means, when the component rerenders and the term has been changed run useEffect code
+    // we can't use async await direct inside useEffect(rather make helper fun with async code and run it inside useEffect)
     useEffect(() => {
-        console.log('first time');
-    })
+        const searchWiki = async () => {
+            await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term,
+                }
+            })
+        }
+        searchWiki()
+
+    }, [term])
+
     return (
         <div>
             <div className='ui form'>
