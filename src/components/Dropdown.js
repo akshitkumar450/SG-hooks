3fbcf9d5234bdc1f74e76bcb33b0a013af1762ed
Akshit Kumar <akshitkumar450@gmail.com>
2021-04-03 18:17:30 +0530

    hide and show dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Dropdown = (props) => {
+    // to show and hide the dropdown
+    // initially dropdown will be hidden
+    const [open, setOpen] = useState(false)
+
     const result = props.options.map((option, index) => {
         // filtering the selected option from the list
         if (option.value === props.selected.value) {
@@ -23,12 +27,19 @@ const Dropdown = (props) => {
                 <label className='label'>
                     select a color
                 </label>
-                <div className='ui selection dropdown visible active'>
+                { /*classes active visible active was responsible for opening the dropdown
+                but now it will open only when the state value of open is true*/    }
+                <div
+                    onClick={() => setOpen(!open)}
+                    className={`ui selection dropdown ${open ? 'visible active' : ''}`}>
                     <i className='dropdown icon'></i>
                     <div className='text'>
                         {props.selected.label}
                     </div>
-                    <div className='menu visible transition'>
+
+                    { /*classes active visible transition was responsible for opening the dropdown
+                    but now it will open only when the state value of open is true*/    }
+                    <div className={`menu ${open ? 'visible transition' : ''}`}>
                         {result}
                     </div>
                 </div>
